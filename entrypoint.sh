ca8048e767c9f5ef558425e30611007ef48cd511
Joseph <JMcDade42@gmail.com>
2023-08-08 01:48:18 +0000

    Enable all of entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 # Check if PUID needs to be updated for sduser
 
-# local changed=false
+local changed=false
 
-# current_puid=$(id -u sduser)
-# if [[ $current_puid != $PUID ]]; then
-#   usermod -u "$PUID" sduser
-#   changed=true
-# fi
+# Check if PUID needs to be updated for sduser
+current_puid=$(id -u sduser)
+if [[ $current_puid != $PUID ]]; then
+  usermod -u "$PUID" sduser
+  changed=true
+fi
 
-# # Check if PGID needs to be updated for sdgroup
-# current_pgid=$(getent group sdgroup | cut -d: -f3)
-# if [[ $current_pgid != $PGID ]]; then
-#   groupmod -g "$PGID" sdgroup
-#   changed=true
-# fi
+# Check if PGID needs to be updated for sdgroup
+current_pgid=$(getent group sdgroup | cut -d: -f3)
+if [[ $current_pgid != $PGID ]]; then
+  groupmod -g "$PGID" sdgroup
+  changed=true
+fi
 
-# if [[ $changed == true ]]; then
-#   find /app -mount -exec chown sduser:sdgroup {} \;
-# fi
+if [[ $changed == true ]]; then
+  find /app -mount -exec chown sduser:sdgroup {} \;
+fi
 
 # Run webui.sh as the sduser
 exec gosu sduser /app/stable-diffusion-webui/webui.sh "$@"
